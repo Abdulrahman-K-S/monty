52a19919abfe32fac785c27b3512c00bd69658a5
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-10-20 00:52:14 +0300

    Added 'op_nop.c'

---
+++ op_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * op_nop - A function that doesn't do anything.
+ *
+ * @stack: The head of the stack.
+ *
+ * Return: Nothing.
+*/
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+    (void) stack;
+    (void) line_number;
+}
