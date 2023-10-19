96fcfa1929181a37eaf4eca38103c262d6c161d1
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-10-20 01:46:23 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,27 +1,32 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_
+#define _MONTY_
 
-#define USING_STACK 300
-#define USING_QUEUE 301
+/* Constants */
+#define SUCSS_OP		0
+#define VALID_PARM		0
+#define MIN_ARGS		2
+#define METH_STACK		300
+#define METH_QUEUE		301
 
-/* Common errors */
-#define UNKNOWN_INSTRUCTION 100 /*L%d: unknown instruction %s\n*/
-#define BAD_MALLOC 101 /*Error: malloc failed\n*/
+/* Common Errors */
+#define UNKNOWN_INSTRUCTION	100
+#define BAD_MALLOC	101
+#define USAGE_ERR	102
 
-/* Interpreter errors */
-#define USAGE_ERR 200 /*USAGE: monty file*/
-#define PUSH_ERR 201 /*L%d: usage: push integer\n*/
-#define PINT_ERR 202 /*L%d: can't pint, stack empty\n*/
-#define POP_ERR 203 /*L%d: can't pop an empty stack\n*/
-#define SWAP_ERR 204 /*L%d: can't swap, stack too short\n*/
-#define ADD_ERR 205 /*L%d: can't add, stack too short\n*/
-#define SUB_ERR 206 /*L%d: can't sub, stack too short\n*/
-#define DIV_ERR 207 /*L%d: can't div, stack too short\n*/
-#define DIV_ERR_ZERO 208 /*L%d: division by zero\n*/
-#define MUL_ERR 209 /*L%d: can't mul, stack too short\n*/
-#define MOD_ERR 210 /*L%d: can't mod, stack too short\n*/
-#define PCHAR_ERR_USAGE 211 /*L%d: can't pchar, value out of range\n*/
-#define PCHAR_ERR 212 /*L%d: can't mod, stack too short\n*/
+/* Usage Errors */
+#define ERR_ARG_USG		200
+#define PUSH_ERR	201
+#define PINT_ERR	202
+#define POP_ERR		203
+#define SWAP_ERR	204
+#define ADD_ERR		205
+#define SUB_ERR		206
+#define DIV_ERR		207
+#define DIV_ERR_ZERO		208
+#define MUL_ERR		209
+#define MOD_ERR		210
+#define PCHAR_ERR_USAGE		211
+#define PCHAR_ERR		212
 
 #include <ctype.h>
 #include <fcntl.h>
@@ -37,7 +42,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -52,7 +57,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -60,11 +65,31 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *stack;
+extern stack_t *head;
 
-/* op-code Functions */
-void op_push_start(stack_t **stack, unsigned int param);
-void op_push_end(stack_t **stack, const int param);
+void check_args_num(int argn);
+FILE *open_file(char *filename);
+void check_access_rights(char *filename);
+int check_push_param(char *param);
+int check_digits(char *s);
+
+/* Stack utilities */
+void frees_stack(void);
+unsigned int count_stack(stack_t *stack);
+
+/* Error handlers */
+void handle_error(int errno, char *opcode, unsigned int line, char *buff);
+void handle_cerror(int errno, char *opcode, unsigned int line);
+void handle_uerror(int errno, unsigned int line);
+void handle_more_uerror(int errno, unsigned int line);
+
+/* Execution functions */
+int handle_execution(char *op_code, char *op_param, unsigned int line, int m);
+void (*pick_func(char *s))(stack_t **, unsigned int);
+
+/* op_codes Functions */
+void op_push(stack_t **stack, unsigned int param);
+void op_push_queue(stack_t **stack, unsigned int param);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
@@ -72,26 +97,12 @@ void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
 void op_sub(stack_t **stack, unsigned int line_number);
-void op_div(stack_t **stack, unsigned int line_number);
+void op_divide(stack_t **stack, unsigned int line_number);
 void op_mul(stack_t **stack, unsigned int line_number);
 void op_mod(stack_t **stack, unsigned int line_number);
 void op_pchar(stack_t **stack, unsigned int line_number);
 void op_pstr(stack_t **stack, unsigned int line_number);
 void op_rotl(stack_t **stack, unsigned int line_number);
 void op_rotr(stack_t **stack, unsigned int line_number);
-
-/* Stack utilities */
-void free_stack(stack_t *stack);
-unsigned int count_stack(stack_t *stack);
-
-/* File functions */
-FILE *open_file(char *filename);
-void access_rights(char *filename);
-
-/* Handle error functions*/
-void handle_error(int errno, char *op_code, unsigned int line, char *buff);
-void handle_cerror(int errno, char *opcode, unsigned int line);
-void handle_uerror(int errno, unsigned int line);
-void handle_more_uerror(int errno, unsigned int line);
 
 #endif
