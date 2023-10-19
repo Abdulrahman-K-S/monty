521412c84660f579b63f5c8755c38bd494057d69
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-10-20 00:57:45 +0300

    Added 'op_mul.c'

---
+++ op_mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * op_mul - A function that multiplies the top two elements.
+ *
+ * @stack: The head of the stack.
+ *
+ * Return: Nothing.
+*/
+void op_mul(stack_t **stack, unsigned int line_number)
+{
+    unsigned int temp = 0, length = 0;
+
+    length = count_stack(*stack);
+    if (length < 2)
+        handle_error(MUL_ERR, NULL, line_number, NULL);
+
+    if ((*stack)->next != NULL)
+    {
+        temp = (*stack)->next->n * (*stack)->n;
+        (*stack)->next->n = temp;
+        op_pop(stack);
+        return;
+    }
+}
